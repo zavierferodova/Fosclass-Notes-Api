047cde9d5b604eca8e7ef91f05abcaf38eb46220
Zavier Ferodova <zavierferodova@gmail.com>
2023-12-02 10:33:48 +0700

    Add notes controller implementation

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -1,12 +1,11 @@
-// const Note = require('../models/note')
+const Note = require('../models/note')
 const responses = require('../constants/responses')
 const apiResponse = require('../helpers/response-helper')
 
 const getNotes = async (req, res) => {
   try {
-    /**
-     * @todo Write your code here
-     */
+    const notes = await Note.findAll()
+    return apiResponse(res, responses.success.code, 'Notes retrieved successfully', notes)
   } catch (error) {
     return apiResponse(res, responses.error.code, 'Error retrieving notes')
   }
@@ -14,9 +13,13 @@ const getNotes = async (req, res) => {
 
 const getNote = async (req, res) => {
   try {
-    /**
-     * @todo Write your code here
-     */
+    const { id } = req.params
+    const note = await Note.findByPk(id)
+    if (note) {
+      return apiResponse(res, responses.success.code, 'Note retrieved successfully', note)
+    } else {
+      return apiResponse(res, responses.notFound.code, 'Note not found')
+    }
   } catch (error) {
     return apiResponse(res, responses.error.code, 'Error retrieving note')
   }
@@ -24,9 +27,14 @@ const getNote = async (req, res) => {
 
 const createNote = async (req, res) => {
   try {
-    /**
-     * @todo Write your code here
-     */
+    const { title, content } = req.body
+    const note = await Note.create({
+      title,
+      content,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    return apiResponse(res, responses.created.code, 'Note created successfully', note)
   } catch (error) {
     return apiResponse(res, responses.error.code, 'Error creating note')
   }
@@ -34,9 +42,18 @@ const createNote = async (req, res) => {
 
 const updateNote = async (req, res) => {
   try {
-    /**
-     * @todo Write your code here
-     */
+    const { title, content } = req.body
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+      await note.update({
+        title,
+        content,
+        updatedAt: new Date()
+      })
+      return apiResponse(res, responses.success.code, 'Note updated successfully', note)
+    } else {
+      return apiResponse(res, responses.notFound.code, 'Note not found')
+    }
   } catch (error) {
     return apiResponse(res, responses.error.code, 'Error updating note')
   }
@@ -44,9 +61,16 @@ const updateNote = async (req, res) => {
 
 const deleteNote = async (req, res) => {
   try {
-    /**
-     * @todo Write your code here
-     */
+    const { id } = req.params
+    const note = await Note.findByPk(id)
+    if (note) {
+      await note.destroy()
+      return apiResponse(res, responses.success.code, 'Note deleted successfully', {
+        id: note.id
+      })
+    } else {
+      return apiResponse(res, responses.notFound.code, 'Note not found')
+    }
   } catch (error) {
     return apiResponse(res, responses.error.code, 'Error deleting note')
   }
